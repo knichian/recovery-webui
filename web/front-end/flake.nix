40d15192731ec198593273670335402021fe4a09
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2026-06-10 17:28:44 -0300

    Feat: Add Node as a dependency

---
+++ web/front-end/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dependencies for recovery-web-ui frontend";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.nodejs
+      ];
+    };
+  };
+}
